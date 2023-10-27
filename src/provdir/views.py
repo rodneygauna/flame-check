2df3ead26741cc51cad7ad11c7c4bf19b5f6fc14
Rodney Gauna <rodneygauna@gmail.com>
2023-10-27 16:06:35 -0700

    start of healthcareservice

--- src/provdir/views.py
+++ src/provdir/views.py
@@ -87,3 +87,59 @@ def test_endpoints():
 
     return render_template('provdir/test_results.html',
                            results=results)
+
+
+# Route - Test HealthcareService Search Paramters
+@provdir_bp.route('/provdir/healthcareservice_test', methods=['GET'])
+def healthcareservice_test():
+    """Test HealthcareService Search Parameters"""
+    # Get the base URL from the query string
+    base_url = request.args.get('base_url')
+
+    # Search Parameters to test
+    search_parameters = [
+        'location',
+        # 'coverage-area',
+        'organization',
+        #'endpoint',
+        'name',
+        'service-category',
+        'service-type',
+        'specialty',
+        '_id',
+        '_lastUpdated',
+    ]
+
+    # Navigate to the endpoint and store the first entry in the results
+    results = {}
+    try:
+        response = requests.get(
+            base_url + f"fhirprovdir/HealthcareService", timeout=15)
+        response.raise_for_status()
+        data = response.json()
+        first_entry = data['entry'][0]
+        # Return the data for the search parameters to results
+        results['location'] = first_entry['location'][0]['reference']
+        #results['coverage-area'] = first_entry['coverageArea'][0]['reference']
+        results['organization'] = first_entry['providedBy']['reference']
+        #results['endpoint'] = first_entry['endpoint'][0]['reference']
+        results['name'] = first_entry['name']
+        results['service-category'] = first_entry['category'][0]['coding'][0]['code']
+        results['service-type'] = first_entry['extension'][0]['extension'][0]['valueCodeableConcept']['coding'][0]['code']
+        results['specialty'] = first_entry['specialty'][0]['coding'][0]['code']
+        results['_id'] = first_entry['id']
+        results['_lastUpdated'] = first_entry['meta']['lastUpdated']
+    except requests.exceptions.RequestException as e:
+        results['location'] = f"Failed: {e}"
+        #results['coverage-area'] = f"Failed: {e}"
+        results['organization'] = f"Failed: {e}"
+        #results['endpoint'] = f"Failed: {e}"
+        results['name'] = f"Failed: {e}"
+        results['service-category'] = f"Failed: {e}"
+        results['service-type'] = f"Failed: {e}"
+        results['specialty'] = f"Failed: {e}"
+        results['_id'] = f"Failed: {e}"
+        results['_lastUpdated'] = f"Failed: {e}"
+
+    return render_template('provdir/test_results.html',
+                           results=results)
