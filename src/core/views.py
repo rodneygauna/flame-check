900db365d8cc437c210a97128172a2ce1f6b6fe6
Rodney Gauna <rodneygauna@gmail.com>
2023-10-27 16:31:17 +0000

    lint fix

--- src/core/views.py
+++ src/core/views.py
@@ -6,8 +6,10 @@ Core views for the application
 # Imports
 from flask import render_template, Blueprint
 
+
 # Blueprint
 core_bp = Blueprint('core', __name__)
+
 
 # Route - Homepage
 @core_bp.route('/')
