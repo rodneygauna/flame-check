0495e2d4a0bb6436aeedc4645b718c162fa58113
Rodney Gauna <rodneygauna@gmail.com>
2023-10-27 21:54:08 -0700

    code cleaned

--- src/provdir/views.py
+++ src/provdir/views.py
@@ -9,6 +9,14 @@ from flask import (
     render_template, Blueprint, request, redirect, url_for
 )
 
+from src.provdir.test_connection import test_connection
+from src.provdir.endpoint_totals import get_endpoint_total
+from src.provdir.healthservice import (
+    get_first_entry, get_location, get_organization, get_endpoint,
+    get_name, get_service_category, get_service_type, get_specialty,
+    get_id, get_last_updated
+)
+
 # Blueprint
 provdir_bp = Blueprint('provdir', __name__)
 
@@ -18,39 +26,29 @@ provdir_bp = Blueprint('provdir', __name__)
 def index():
     """Landing Page"""
 
-    connection_result = None
-    connection_successful = False
-
     if request.method == 'POST':
         base_url = request.form.get('base_url')
 
-        # Ensure the URL starts with https://
+        # Ensure the URL starts with https:// and ends with a slash
         if not base_url.startswith('https://'):
-            base_url = f"https://{base_url}"
-        # Ensure the URL ends with a slash
+            base_url = f'https://{base_url}'
         if not base_url.endswith('/'):
-            base_url += '/'
+            base_url = f'{base_url}/'
 
         api_endpoint = f"{base_url}fhirprovdir/Practitioner"
 
-        try:
-            response = requests.get(
-                api_endpoint,
-                timeout=10
-            )
-            response.raise_for_status()
-            connection_result = "Connection successful (HTTP 200 OK)"
-            connection_successful = True
-        except requests.exceptions.RequestException as e:
-            connection_result = f"Connection failed: {e}"
+        connection_result = test_connection(api_endpoint)
 
-        if connection_successful:
+        if connection_result['success']:
             return redirect(url_for('provdir.test_endpoints',
                                     base_url=base_url))
+        else:
+            return render_template('provdir/index.html',
+                                   title='Flame Check - Provider Directory',
+                                   connection_result=connection_result['message'])
 
     return render_template('provdir/index.html',
-                           title='Flame Check - Provider Directory',
-                           connection_result=connection_result)
+                           title='Flame Check - Provider Directory')
 
 
 # Route - Test Endpoints
@@ -71,22 +69,10 @@ def test_endpoints():
     ]
 
     # Store results in a dictionary
-    results = {}
+    results = {endpoint: get_endpoint_total(
+        base_url, endpoint) for endpoint in endpoints}
 
-    for endpoint in endpoints:
-        try:
-            response = requests.get(
-                base_url + f"fhirprovdir/{endpoint}", timeout=15)
-            response.raise_for_status()
-            data = response.json()
-            total = data['total']
-            # Return the data and total to results
-            results[endpoint] = total
-        except requests.exceptions.RequestException as e:
-            results[endpoint] = f"Failed: {e}"
-
-    return render_template('provdir/test_results.html',
-                           results=results)
+    return render_template('provdir/test_results.html', results=results)
 
 
 # Route - Test HealthcareService Search Paramters
@@ -96,52 +82,25 @@ def healthcareservice_test():
     # Get the base URL from the query string
     base_url = request.args.get('base_url')
 
-    # Navigate to the endpoint and store the first entry in the results
-    results = {}
+    # Get the first entry from the endpoint
     try:
-        response = requests.get(
-            base_url + "fhirprovdir/HealthcareService", timeout=15)
-        response.raise_for_status()
-        data = response.json()
-        first_entry = data['entry'][0]
-        try:
-            results['location'] = first_entry['resource']['location'][0]['reference']
-        except KeyError:
-            results['location'] = "Location not found"
-        try:
-            results['organization'] = first_entry['resource']['providedBy']['reference']
-        except KeyError:
-            results['organization'] = "Organization not found"
-        try:
-            results['endpoint'] = first_entry['resource']['endpoint'][0]['reference']
-        except KeyError:
-            results['endpoint'] = "Endpoint not found"
-        try:
-            results['name'] = first_entry['resource']['name']
-        except KeyError:
-            results['name'] = "Name not found"
-        try:
-            results['service-category'] = first_entry['resource']['category'][0]['coding'][0]['code']
-        except KeyError:
-            results['service-category'] = "Service Category not found"
-        try:
-            results['service-type'] = first_entry['resource']['extension'][0]['extension'][0]['valueCodeableConcept']['coding'][0]['code']
-        except KeyError:
-            results['service-type'] = "Service Type not found"
-        try:
-            results['specialty'] = first_entry['resource']['specialty'][0]['coding'][0]['code']
-        except KeyError:
-            results['specialty'] = "Specialty not found"
-        try:
-            results['_id'] = first_entry['resource']['id']
-        except KeyError:
-            results['_id'] = "ID not found"
-        try:
-            results['_lastUpdated'] = first_entry['resource']['meta']['lastUpdated']
-        except KeyError:
-            results['_lastUpdated'] = "Last Updated not found"
+        first_entry = get_first_entry(base_url)
     except requests.exceptions.RequestException as e:
-        results['error'] = f"Failed: {e}"
+        results = {'error': str(e)}
+        return render_template('provdir/test_results.html', results=results)
+
+    # Get the various fields from the first entry
+    results = {
+        'location': get_location(first_entry),
+        'organization': get_organization(first_entry),
+        'endpoint': get_endpoint(first_entry),
+        'name': get_name(first_entry),
+        'service-category': get_service_category(first_entry),
+        'service-type': get_service_type(first_entry),
+        'specialty': get_specialty(first_entry),
+        '_id': get_id(first_entry),
+        '_lastUpdated': get_last_updated(first_entry)
+    }
 
     return render_template('provdir/test_results.html',
                            results=results)
